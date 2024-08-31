8e288462f78cf0c9ea870f090dc565035c960c6f
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-01 07:49:42 +0900

    feat: update eslint config

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -2,12 +2,6 @@ const { resolve } = require('node:path')
 
 const project = resolve(process.cwd(), 'tsconfig.json')
 
-/*
- * This is a custom ESLint configuration for use with
- * internal (bundled by their consumer) libraries
- * that utilize React.
- */
-
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   parser: '@typescript-eslint/parser',
@@ -20,15 +14,4 @@ module.exports = {
     },
   },
   ignorePatterns: ['node_modules/', 'dist/'],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    browser: true,
-  },
-  overrides: [
-    // Force ESLint to detect .tsx files
-    { files: ['*.js?(x)', '*.ts?(x)'] },
-  ],
 }
