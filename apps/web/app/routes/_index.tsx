2811533fed67618c51ef3d0d9edf60fe48ededbd
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-02 00:16:45 +0900

    refactor: merge remix app with cf template (#37)
    * merge git ignore
    
    * merge eslint
    
    * merge vite config
    
    * merge tsconfig
    
    * merge package.json
    
    * merge context
    
    * merge app dir
    
    * move functions dir
    
    * merge README.md
    
    * merge tailwind.config.ts
    
    * merge public
    
    * merge app
    
    * update

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,48 +1,56 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/cloudflare'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: 'New Remix App' },
+    {
+      name: 'description',
+      content: 'Welcome to Remix on Cloudflare!',
+    },
+  ]
+}
+
+import { json } from '@remix-run/cloudflare'
+import { useLoaderData } from '@remix-run/react'
+import { Button } from '@repo/ui/button'
+
+export const loader = async () => {
+  const message = 'Hello from the loader'
+  return json({ message })
+}
 
 export default function Index() {
+  const { message } = useLoaderData<typeof loader>()
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
+    <div className='font-sans p-4'>
+      <h1 className='text-3xl'>Welcome to Remix on Cloudflare</h1>
+      <div>message: {message}</div>
+      <Button appName='cloudflare app' className='border'>
+        Click Me!
+      </Button>
+      <ul className='list-disc mt-4 pl-6 space-y-2'>
         <li>
           <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
+            className='text-blue-700 underline visited:text-purple-900'
+            target='_blank'
+            href='https://remix.run/docs'
+            rel='noreferrer'
           >
             Remix Docs
           </a>
         </li>
+        <li>
+          <a
+            className='text-blue-700 underline visited:text-purple-900'
+            target='_blank'
+            href='https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/'
+            rel='noreferrer'
+          >
+            Cloudflare Pages Docs - Remix guide
+          </a>
+        </li>
       </ul>
     </div>
-  );
+  )
 }
