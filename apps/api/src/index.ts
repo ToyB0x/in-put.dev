72b5ea304578dabaf8c697b2ac164dea8a99a8b0
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-02 05:21:50 +0900

    use db

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,35 @@
 import { Hono } from 'hono'
+import { drizzle } from 'drizzle-orm/d1'
+import { users } from '@repo/database'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Env }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  const db = drizzle(c.env.DB_TEST1)
+  const result = await db.select().from(users).all()
+
+  return c.json({ message: 'Hello Hono!', result })
 })
+
+// import { z } from 'zod'
+// import { zValidator } from '@hono/zod-validator'
+// app.post(
+//   '/',
+//   (c) =>
+//     zValidator(
+//       'json',
+//       z.object({
+//         url: z.string().url(),
+//       }),
+//     ),
+//   (c) => {
+//     return c.json(
+//       {
+//         success: true,
+//       },
+//       201,
+//     )
+//   },
+// )
 
 export default app
