a18845db556859eac0a9cef2225015f99eb06c9b
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-01 22:02:49 +0900

    feat: add cloudflare pages (#33)
    * update deps
    
    * add remix for cloudflare
    
    * add cloudflare configs
    
    * rename dir
    
    * update lockfile
    
    * feat: add cloudflare pages
    
    * fix lint

---
+++ apps/cloudflare-web/app/routes/_index.tsx
@@ -0,0 +1,52 @@
+import type { MetaFunction } from '@remix-run/cloudflare'
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'New Remix App' },
+    {
+      name: 'description',
+      content: 'Welcome to Remix on Cloudflare!',
+    },
+  ]
+}
+
+import { json } from '@remix-run/cloudflare'
+import { useLoaderData } from '@remix-run/react'
+
+export const loader = async () => {
+  const message = 'Hello from the loader 2!'
+  return json({ message })
+}
+
+export default function Index() {
+  const { message } = useLoaderData<typeof loader>()
+
+  return (
+    <div className='font-sans p-4'>
+      <h1 className='text-3xl'>Welcome to Remix on Cloudflare</h1>
+      <div>message: {message}</div>
+      <ul className='list-disc mt-4 pl-6 space-y-2'>
+        <li>
+          <a
+            className='text-blue-700 underline visited:text-purple-900'
+            target='_blank'
+            href='https://remix.run/docs'
+            rel='noreferrer'
+          >
+            Remix Docs
+          </a>
+        </li>
+        <li>
+          <a
+            className='text-blue-700 underline visited:text-purple-900'
+            target='_blank'
+            href='https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/'
+            rel='noreferrer'
+          >
+            Cloudflare Pages Docs - Remix guide
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
