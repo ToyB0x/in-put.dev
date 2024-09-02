f100f962fe124a85a8520dab4ec29d5e4e8f8fdf
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-02 09:58:25 +0900

    feat: url creation api (#46)
    * feat: add insert url api
    
    * feat: add url schema

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,50 @@
 import { Hono } from 'hono'
 import { drizzle } from 'drizzle-orm/d1'
-import { users } from '@repo/database'
+import { urls, users } from '@repo/database'
 
 const app = new Hono<{ Bindings: Env }>()
 
+// List all urls
 app.get('/', async (c) => {
   const db = drizzle(c.env.DB_TEST1)
-  const result = await db.select().from(users).all()
+  const result = await db.select().from(urls).all()
+  return c.json({ message: 'URL list', result })
+})
 
-  return c.json({ message: 'Hello Hono!', result })
+// List all users
+app.get('/users', async (c) => {
+  const db = drizzle(c.env.DB_TEST1)
+  const result = await db.select().from(users).all()
+  return c.json({ result })
+})
+
+// Insert user
+// TODO: change to post method
+app.get('/users/new', async (c) => {
+  const db = drizzle(c.env.DB_TEST1)
+  const result = await db
+    .insert(users)
+    .values({ displayName: 'User1', email: 'user1@example.com' })
+    .onConflictDoNothing()
+  return c.json({ result })
+})
+
+// Insert url
+// NOTE: Use get method for arriving user via bookmarklet
+app.get('/add/:url', async (c) => {
+  const db = drizzle(c.env.DB_TEST1)
+
+  // TODO: zod validation url type
+  const url = c.req.param('url')
+
+  // TODO: fetch title from url
+  const pageTitle = 'Title1'
+
+  // TODO: set user_id
+  const userId = 1
+
+  await db.insert(urls).values({ url, pageTitle, userId }).onConflictDoNothing()
+  return c.json({ message: 'Inserted', url })
 })
 
 // import { z } from 'zod'
