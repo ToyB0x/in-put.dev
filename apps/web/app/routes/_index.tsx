aeedfd7e4a0fc0750d4db5c37fef6485bf8109b1
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-03 06:15:51 +0900

    feat: list UI (#53)
    * feat: update vite config
    
    * update index

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,8 +1,7 @@
 import { json, type MetaFunction, type LoaderFunctionArgs } from '@remix-run/cloudflare'
 import { useLoaderData } from '@remix-run/react'
 import { drizzle } from 'drizzle-orm/d1'
-import { Button } from '@repo/ui/button'
-import { users } from '@repo/database'
+import { urls } from '@repo/database'
 
 export const meta: MetaFunction = () => {
   return [
@@ -16,46 +15,35 @@ export const meta: MetaFunction = () => {
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
   const db = drizzle(context.cloudflare.env.DB_TEST1)
-  const result = await db.select().from(users).all()
+  const result = await db.select().from(urls).all()
 
   const message = 'Hello from the loader'
   return json({ message, result })
 }
 
 export default function Index() {
-  const { message, result } = useLoaderData<typeof loader>()
+  const { result } = useLoaderData<typeof loader>()
+  const uniqueHosts = [...new Set(result.map((url) => new URL(url.url).host))]
 
   return (
-    <div className='font-sans p-4'>
-      <h1 className='text-3xl'>Welcome to Remix on Cloudflare</h1>
-      <div>message: {message}</div>
-      <div>result: {result.length}</div>
-      <div>result: {JSON.stringify(result)}</div>
-      <Button appName='cloudflare app' className='border'>
-        Click Me!
-      </Button>
-      <ul className='list-disc mt-4 pl-6 space-y-2'>
-        <li>
-          <a
-            className='text-blue-700 underline visited:text-purple-900'
-            target='_blank'
-            href='https://remix.run/docs'
-            rel='noreferrer'
-          >
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <a
-            className='text-blue-700 underline visited:text-purple-900'
-            target='_blank'
-            href='https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/'
-            rel='noreferrer'
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-      </ul>
+    <div className='font-sans p-8'>
+      <h1 className='text-4xl'>Input.dev</h1>
+      <ol className='mt-2'>
+        {uniqueHosts.sort().map((host) => (
+          <li key={host} className='p-2 py-4'>
+            <p>
+              {host}
+              <ol className='ml-8 list-disc'>
+                {result
+                  .filter((url) => new URL(url.url).host === host)
+                  .map((url) => (
+                    <li key={url.id}>{decodeURIComponent(url.url)}</li>
+                  ))}
+              </ol>
+            </p>
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [
     removeUseClient(),
-    remixCloudflareDevProxy(),
+    remixCloudflareDevProxy({ persist: { path: '../../cf-local-worker/v3' } }),
     remix({
       future: {
         v3_fetcherPersist: true,
