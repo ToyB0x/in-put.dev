35079f342339c04b674e20e7d47f70e52346d9da
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-07 20:41:23 +0900

    remove username default

--- packages/database/schema/users.ts
+++ packages/database/schema/users.ts
@@ -6,7 +6,7 @@ export const users = sqliteTable(
   'users',
   {
     id: integer('id').primaryKey(),
-    name: text('name').notNull().default('ToyB0x'),
+    name: text('name').notNull(),
     email: text('email').notNull(),
     displayName: text('displayName'),
     createdAt: text('createdAt')
