411313de49e929c9d85680b59b37f792daff693f
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-09 04:45:23 +0900

    feat: login (#78)

--- apps/web/app/components/LoginFromBrowser.tsx
+++ apps/web/app/components/LoginFromBrowser.tsx
@@ -1,18 +1,15 @@
 'use client' // for firebase browser
 
 import { Form, useFetcher } from '@remix-run/react'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { firebaseAuthBrowser } from '@/.client/firebase'
 
-export const SignupFromBrowser: React.FC = () => {
+export const LoginFromBrowser: React.FC = () => {
   const fetcher = useFetcher()
 
   // TODO: don't show in url bar
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     const formData = new FormData(e.target as HTMLFormElement)
-
-    const userName = formData.get('userName')
-    if (typeof userName !== 'string') throw Error('userName is invalid')
 
     const email = formData.get('email')
     if (typeof email !== 'string') throw Error('email is invalid')
@@ -20,17 +17,16 @@ export const SignupFromBrowser: React.FC = () => {
     const password = formData.get('password')
     if (typeof password !== 'string') throw Error('password is invalid')
 
-    const credential = await createUserWithEmailAndPassword(firebaseAuthBrowser, email, password)
+    const credential = await signInWithEmailAndPassword(firebaseAuthBrowser, email, password)
     const { refreshToken } = credential.user
     const idToken = await credential.user.getIdToken()
 
     // Submit key/value JSON as a FormData instance
-    fetcher.submit({ userName, idToken, refreshToken }, { method: 'POST' })
+    fetcher.submit({ idToken, refreshToken }, { method: 'POST' })
   }
 
   return (
     <Form onSubmit={onSubmit} className='flex flex-col'>
-      <input name='userName' type='text' placeholder='userName' />
       <input name='email' type='email' placeholder='email' />
       <input name='password' type='password' placeholder='password' />
       <button type='submit'>send</button>
