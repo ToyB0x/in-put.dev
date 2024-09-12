e4d15c383a759c56e79251446f7cdab17cb93064
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-13 03:00:56 +0900

    update dizzle config

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const dbUrl =
+  process.env.NEON_BRANCH === 'MAIN'
+    ? process.env.NEON_DATABASE_URL_BRANCH_MAIN
+    : process.env.NEON_DATABASE_URL_BRANCH_LOCAL
+if (!dbUrl) throw Error('Database URL not found')
 
 export default defineConfig({
   schema: './schema',
   dialect: 'postgresql',
-  dbCredentials: {
-    // host: 'localhost',
-    // port: 5433,
-    // user: 'user',
-    // password: 'pass',
-    // database: 'inputs',
-    // ssl: false, // can be boolean | "require" | "allow" | "prefer" | "verify-full" | options from node:tls
-
-    // url: "postgresql://postgres:password@host:port/db",
-    // url: "postgresql://user:pass@localhost:5433/inputs",
-    url: 'postgresql://inputs_owner:Y2J9HSnaMtRd@ep-flat-grass-a1hwukb3-pooler.ap-southeast-1.aws.neon.tech/inputs?sslmode=require',
-  },
-  verbose: true,
   strict: true,
+  verbose: true,
+  dbCredentials: {
+    url: dbUrl,
+  },
 })
