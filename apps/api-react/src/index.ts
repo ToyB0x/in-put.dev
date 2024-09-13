cf150a6a34c2d3977d5a199cfba1a2deeade9142
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-13 13:14:56 +0900

    add comment

--- apps/api-react/src/index.ts
+++ apps/api-react/src/index.ts
@@ -7,6 +7,7 @@ const app = new Hono<{ Bindings: Env }>()
 // List all urls
 app.get('/', async (c) => {
   const db = drizzle(c.env.DB_TEST_API_REACT_1)
+  // workaround for avoiding type d1 / neon db mismatch error (maybe pnpm package linking issue or drizzle-orm related package issue)
   // @ts-ignore
   const result = await db.select().from(users).all()
   return c.json(result)
