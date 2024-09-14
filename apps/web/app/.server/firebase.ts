6a1992e86d224bdabbb5377f81bc75c1b0fc9ec8
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-15 06:46:32 +0900

    feat: add env package (#100)
    * feat: add env package
    
    * remove index

--- apps/web/app/.server/firebase.ts
+++ apps/web/app/.server/firebase.ts
@@ -1,4 +1,3 @@
-import { VITE_PUBLIC_FIREBASE_BROWSER_API_KEY, VITE_PUBLIC_FIREBASE_PROJECT_ID } from '@/env-public'
 import {
   Auth,
   ServiceAccountCredential,
@@ -7,12 +6,13 @@ import {
 } from 'firebase-auth-cloudflare-workers'
 import { firebaseSessionCookieExpiresIn } from '@/.server/cookie'
 import type { FirebaseOptions } from 'firebase/app'
+import { sharedPublicViteEnv } from '@repo/env/shared'
 
 // Your web app's Firebase configuration
 // ref: https://stackoverflow.com/a/37484053
 const firebaseConfig = {
-  projectId: VITE_PUBLIC_FIREBASE_PROJECT_ID,
-  apiKey: VITE_PUBLIC_FIREBASE_BROWSER_API_KEY, // public browser endpoint
+  projectId: sharedPublicViteEnv.VITE_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: sharedPublicViteEnv.VITE_PUBLIC_FIREBASE_BROWSER_API_KEY, // public browser endpoint
 } satisfies FirebaseOptions
 
 // NOTE: Avoid wrong kid error
--- apps/web/app/env-public.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Embed public envs on build step by vite
- * Don't commit PRIVATE envs (If you need to add PRIVATE envs, add them from cloudflare console page or via .dev.vars file)
- */
-
-export const VITE_MODE = import.meta.env.MODE as 'development' | 'production' // ref: https://ja.vitejs.dev/guide/env-and-mode
-export const VITE_PUBLIC_FIREBASE_PROJECT_ID = import.meta.env.VITE_PUBLIC_FIREBASE_PROJECT_ID as string
-export const VITE_PUBLIC_FIREBASE_BROWSER_API_KEY = import.meta.env.VITE_PUBLIC_FIREBASE_BROWSER_API_KEY as string
