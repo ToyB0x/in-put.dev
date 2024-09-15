e95f3d49923be478d24149a9a45bd4b519224c3e
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-16 06:14:10 +0900

    update env

--- packages/env/src/shared.ts
+++ packages/env/src/shared.ts
@@ -3,12 +3,13 @@
  * Don't commit PRIVATE envs (If you need to add PRIVATE envs, add them from cloudflare console page or via .dev.vars file)
  */
 
-import { union, object, literal, string, parse } from "valibot"
+import { union, object, literal, string, parse } from 'valibot'
 
 const sharedPublicViteEnvSchema = object({
   MODE: union([literal('development'), literal('production')]), // ref: https://ja.vitejs.dev/guide/env-and-mode
   VITE_PUBLIC_FIREBASE_PROJECT_ID: string(),
   VITE_PUBLIC_FIREBASE_BROWSER_API_KEY: string(), // public browser key
+  VITE_PUBLIC_API_SERVER_URL: string(), // public browser key
 })
 
 export const sharedPublicViteEnv = parse(sharedPublicViteEnvSchema, import.meta.env)
