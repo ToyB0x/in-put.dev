cc014c18b22fd05fe21e6ff3b7754db0f76b05fd
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-16 08:03:03 +0900

    feat: use firebase admin (#106)
    * feat: use firebase admin
    
    * remove key

--- apps/api/src/url.ts
+++ apps/api/src/url.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
 import { drizzle } from 'drizzle-orm/neon-http'
-import { insertUrlRequestSchema, url } from '@repo/database'
+import { insertUrlRequestSchema, url, user } from '@repo/database'
 import { zValidator } from '@hono/zod-validator'
 import { neon } from '@neondatabase/serverless'
 import { and, eq } from 'drizzle-orm'
+import { getOrInitializeAuth } from './clients'
 
 export const urlRoute = new Hono<{ Bindings: Env }>()
   // Get Url Register status
@@ -16,17 +17,23 @@ export const urlRoute = new Hono<{ Bindings: Env }>()
     }),
     async (c) => {
       const sql = neon(c.env.SECRETS_DATABASE_URL)
-      const db = drizzle(sql)
+      const db = drizzle(sql, { schema: { user } })
 
       const { url: jsonUrl } = c.req.valid('json')
 
       const authHeader = c.req.header('Authorization')
       if (!authHeader) throw Error('no auth header')
       const token = authHeader.replace('Bearer ', '')
-      console.log('token', token)
 
-      // TODO: use real user_id with authentication
-      const userId = 1
+      const authAdmin = await getOrInitializeAuth(c.env)
+      const firebaseUser = await authAdmin.verifyIdToken(token)
+
+      const userInDb = await db.query.user.findFirst({
+        where: eq(user.firebaseUid, firebaseUser.uid),
+        columns: { id: true, userName: true },
+      })
+
+      if (!userInDb) throw Error('User not found')
 
       const result = await db
         .select({
@@ -34,7 +41,7 @@ export const urlRoute = new Hono<{ Bindings: Env }>()
           userId: url.userId,
         })
         .from(url)
-        .where(and(eq(url.userId, userId), eq(url.url, jsonUrl)))
+        .where(and(eq(url.userId, userInDb.id), eq(url.url, jsonUrl)))
 
       return c.json({ exists: result.length > 0 })
     },
