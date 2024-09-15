527f682bc75034e5eca64c23299df9ba952e94ac
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-16 07:03:18 +0900

    fix typecheck

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { urlRoute } from './url'
 import { exampleRoute } from './example'
+import '../worker-configuration' // for avoiding @repo/extension package type check error
 
 const app = new Hono()
 
