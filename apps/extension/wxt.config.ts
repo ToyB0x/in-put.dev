f0fea70c5d923777a90f1ba9869c13a3e4eca2cb
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-15 23:05:18 +0900

    update wxt config

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -6,4 +6,21 @@ export default defineConfig({
   runner: {
     chromiumArgs: ['--user-data-dir=./chrome-data'],
   },
+  manifest: {
+    action: {
+      default_icon: 'icon/icon32.png',
+      default_title: 'add bookmark',
+      default_popup: undefined,
+    },
+    permissions: ['contextMenus'],
+    // TODO: confirm valid content_security_policy
+    // ref: https://firebase.google.com/docs/auth/web/chrome-extension?hl=ja#federated-sign-in
+    // content_security_policy: `
+    //   https://apis.google.com
+    //   https://www.gstatic.com
+    //   https://www.googleapis.com
+    //   https://securetoken.googleapis.com`,
+  },
 })
+
+// Chrome 拡張機能のマニフェスト ファイルで、https://apis.google.com URL を content_security_policy 許可リストに必ず追加してください。
