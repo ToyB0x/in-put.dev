30a4be59c79b161f4d6e28f0afbf2f7974359473
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-16 08:00:52 +0900

    remove key

--- apps/api/src/clients/firebaseAdmin.ts
+++ apps/api/src/clients/firebaseAdmin.ts
@@ -4,5 +4,4 @@ export const getOrInitializeAuth = async (env: Env) =>
   Auth.getOrInitialize(
     env.PUBLIC_FIREBASE_PROJECT_ID,
     WorkersKVStoreSingle.getOrInitialize(env.PUBLIC_JWK_CACHE_KEY, env.PUBLIC_JWK_CACHE_KV),
-    new ServiceAccountCredential(env.SECRETS_SERVICE_ACCOUNT_JSON_STRING),
   )
