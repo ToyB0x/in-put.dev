9a4fb6ce8f877cf6b2be6abed496bb11ddf189d2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-16 20:42:23 +0900

    update content

--- apps/extension/entrypoints/content.ts
+++ apps/extension/entrypoints/content.ts
@@ -1,6 +1,49 @@
+import { storageBookmarkV1 } from '@/entrypoints/sotrage/bookmark.ts'
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  // ref: https://developer.chrome.com/docs/extensions/develop/concepts/match-patterns
+  matches: ['https://*/*', 'http://localhost/*'],
+  async main() {
+    // NOTE: Initialization (Mark bookmarked links)
+    const storeUrls = await storageBookmarkV1.getValue()
+    const aTags = document.getElementsByTagName('a')
+    for (let i = 0; i < aTags.length; i++) {
+      // TODO: improve performance with check related domains only
+      if (storeUrls.includes(aTags[i].href)) {
+        const span = document.createElement('span')
+        span.textContent = ' ✅'
+        span.style.fontSize = '0.76rem'
+        aTags[i].appendChild(span)
+      }
+    }
+
+    // NOTE: Message listener (Fire when bookmark list update)
+    browser.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+      const storeUrls = await storageBookmarkV1.getValue()
+      const aTags = document.getElementsByTagName('a')
+      for (let i = 0; i < aTags.length; i++) {
+        if (storeUrls.includes(aTags[i].href)) {
+          if (aTags[i].textContent?.includes(' ✅')) {
+            continue
+          }
+
+          const span = document.createElement('span')
+          span.textContent = ' ✅'
+          span.style.fontSize = '0.76rem'
+          aTags[i].appendChild(span)
+        }
+      }
+    })
+
+    // NOTE: watcher don't work in content script
+    // storageBookmarkV1.watch((newValue) => {
+    //   const aTags = document.getElementsByTagName('a')
+    //   for (let i = 0; i < aTags.length; i++) {
+    //     if (newValue.includes(aTags[i].href)) {
+    //       aTags[i].style.color = 'red!important'
+    //     }
+    //   }
+    //   console.log('newValue', newValue)
+    // })
   },
-});
+})
