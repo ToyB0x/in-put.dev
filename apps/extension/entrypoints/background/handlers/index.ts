b6f99eeb1f3cc15af076cd81cbee48a96de4ca1b
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-19 12:34:20 +0900

    split auth

--- apps/extension/entrypoints/background/handlers/index.ts
+++ apps/extension/entrypoints/background/handlers/index.ts
@@ -1,2 +1,3 @@
+export * from './handleIconClick'
 export * from './handleTabChange'
 export * from './handleTabUpdate'
--- apps/extension/entrypoints/background/index.ts
+++ apps/extension/entrypoints/background/index.ts
@@ -2,10 +2,7 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth/web-extension
 import { initializeApp } from 'firebase/app'
 import { sharedPublicViteEnv } from '@repo/env/shared'
 import type { LoginMessage, LoginResponse } from '@/entrypoints/messages/login'
-import client from '@/entrypoints/libs/client.ts'
-import { storageBookmarkV1 } from '@/entrypoints/storage/bookmark'
-import { getPureUrl } from '@/entrypoints/libs/getPureUrl.ts'
-import { handleTabChange, handleTabUpdate } from './handlers'
+import { handleIconClick, handleTabChange, handleTabUpdate } from './handlers'
 
 const firebaseAppBrowser = initializeApp({
   projectId: sharedPublicViteEnv.VITE_PUBLIC_FIREBASE_PROJECT_ID,
@@ -17,86 +14,7 @@ const auth = getAuth(firebaseAppBrowser)
 export default defineBackground(() => {
   handleTabChange()
   handleTabUpdate()
-
-  // Register icon click event
-  browser.action.onClicked.addListener(async () => {
-    // TODO: open popup if not logged in
-    if (!auth.currentUser) return
-
-    const token = await auth.currentUser?.getIdToken()
-    if (!token) throw Error('no token')
-
-    const [activeTab] = await browser.tabs.query({ active: true, currentWindow: true })
-    const activeUrl = activeTab.url
-    if (!activeUrl) return
-
-    const activeTitle = activeTab.title
-    if (!activeTitle) return
-
-    // TODO: refactor (split code)
-    const clickIconAction: 'ADD' | 'REMOVE' = (await storageBookmarkV1.getValue()).includes(getPureUrl(activeUrl))
-      ? 'REMOVE'
-      : 'ADD'
-
-    // optimistic update
-    await browser.action.setBadgeText({ text: clickIconAction === 'ADD' ? '✅' : null })
-
-    if (clickIconAction === 'ADD') {
-      const resAdd = await client.urls.add.$post(
-        {
-          json: { url: getPureUrl(activeUrl), pageTitle: activeTitle },
-        },
-        {
-          headers: {
-            Authorization: 'Bearer ' + token,
-          },
-        },
-      )
-      const dataAdd = await resAdd.json()
-
-      // fallback optimistic update
-      await browser.action.setBadgeText({ text: dataAdd.success ? '✅' : null })
-    } else {
-      const resDelete = await client.urls.delete.$post(
-        {
-          json: { url: getPureUrl(activeUrl) },
-        },
-        {
-          headers: {
-            Authorization: 'Bearer ' + token,
-          },
-        },
-      )
-      const dataDelete = await resDelete.json()
-
-      // fallback optimistic update
-      await browser.action.setBadgeText({ text: dataDelete.success ? null : '✅' })
-    }
-
-    // update storage with updated bookmarks
-    const resBookmarks = await client.urls.$get(
-      {},
-      {
-        headers: {
-          Authorization: 'Bearer ' + token,
-        },
-      },
-    )
-    const dataBookmarks = await resBookmarks.json()
-
-    await storageBookmarkV1.setValue(dataBookmarks.urls.map(getPureUrl))
-
-    // for debugging
-    // await browser.tabs.sendMessage(activeTab.id!, { type: 'store-updated' })
-
-    const activeTabHost = new URL(activeUrl).host
-    const relatedTabs = await browser.tabs.query({ url: `*://${activeTabHost}/*` })
-
-    // NOTE: Caution! This ignores error message
-    await Promise.allSettled(
-      relatedTabs.map((tab) => tab.id && browser.tabs.sendMessage(tab.id, { type: 'store-updated' })),
-    )
-  })
+  handleIconClick(auth)
 
   // Register extension icon context menu
   const contextMenuId = browser.contextMenus.create({
