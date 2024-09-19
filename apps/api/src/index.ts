3c0ba7cb504e2109dcd8a635c716d1f6f0d6b641
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-19 11:38:31 +0900

    refactor: split remove example

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,10 @@
 import { Hono } from 'hono'
 import { urlRoute } from './routes'
-import { exampleRoute } from './example'
 import '../worker-configuration.d.ts' // for avoiding @repo/extension package type check error
 
 const app = new Hono()
 
-const routes = app.route('/urls', urlRoute).route('/examples', exampleRoute)
+const routes = app.route('/urls', urlRoute)
 
 export default app
 export type AppType = typeof routes
