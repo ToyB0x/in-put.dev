90eb1583e1e91cbb273b82ba2bcde6c79ff0155f
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-21 09:56:55 +0900

    update index

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from 'hono'
-import { urlRoute } from './routes'
+import { domainRoute, urlRoute } from './routes'
 import '../worker-configuration.d.ts' // for avoiding @repo/extension package type check error
 
 const app = new Hono()
 
-const routes = app.route('/urls', urlRoute)
+const routes = app.route('/domains', domainRoute).route('/urls', urlRoute)
 
 export default app
 export type AppType = typeof routes
