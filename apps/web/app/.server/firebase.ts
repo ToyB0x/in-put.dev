ac7365593a64fd27771047e7bb3044f56faa42b2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-23 07:56:10 +0900

    refactor: remove session cookie auth (#155)

--- apps/web/app/.server/firebase.ts
+++ apps/web/app/.server/firebase.ts
@@ -1,10 +1,4 @@
-import {
-  Auth,
-  ServiceAccountCredential,
-  WorkersKVStoreSingle,
-  type FirebaseIdToken,
-} from 'firebase-auth-cloudflare-workers'
-import { firebaseSessionCookieExpiresIn } from '@/.server/cookie'
+import { Auth, ServiceAccountCredential, WorkersKVStoreSingle } from 'firebase-auth-cloudflare-workers'
 import type { FirebaseOptions } from 'firebase/app'
 import { sharedPublicViteEnv } from '@repo/env/shared'
 
@@ -28,74 +22,3 @@ export const getOrInitializeAuth = async (env: Env) =>
     // dummyStore,
     new ServiceAccountCredential(env.SECRETS_SERVICE_ACCOUNT_JSON_STRING),
   )
-
-export const fetchNewTokenWithRefreshToken = async (
-  refreshToken: string,
-): Promise<{
-  idToken: string
-  refreshToken: string
-  expiresIn: number
-}> => {
-  const res = await fetch(
-    // https://cloud.google.com/identity-platform/docs/use-rest-api
-    `https://securetoken.googleapis.com/v1/token?key=${firebaseConfig.apiKey}`,
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        grant_type: 'refresh_token',
-        refreshToken,
-      }),
-    },
-  )
-
-  const result = await res.json<{
-    id_token: string
-    refresh_token: string
-    expires_in: number
-  }>()
-
-  return {
-    idToken: result.id_token,
-    refreshToken: result.refresh_token,
-    expiresIn: result.expires_in,
-  }
-}
-
-export const verifyAndUpdateSessionCookieWithTokenRefreshIfExpired = async (
-  sessionCookie: string,
-  refreshToken: string,
-  auth: Auth,
-): Promise<{
-  user: FirebaseIdToken
-  newSessionCookie: string // obtaion from idToken exchange
-  newRefreshToken: string
-}> => {
-  try {
-    const user = await auth.verifySessionCookie(sessionCookie)
-    return {
-      user,
-      newSessionCookie: sessionCookie, // reuse sessionCookie if not expired
-      newRefreshToken: refreshToken,
-    }
-  } catch (e) {
-    // NOTE: handle session-cookie-expired error
-    // https://firebase.google.com/docs/auth/admin/errors
-    //   errorInfo: {
-    //     code: 'auth/argument-error', // NOTE: Caution! not 'auth/session-cookie-expired'
-    //         message: 'Decoding Firebase session cookie failed. Make sure you passed the entire string JWT which represents a session cookie. See https://firebase.google.com/docs/auth/admin/manage-cookies for details on how to retrieve a session cookie. err: Error: Incorrect "exp" (expiration time) claim must be a newer than "1726194876" (exp: "1726194492")'
-    if (e instanceof Error && 'code' in e && typeof e.code === 'string' && e.code === 'auth/argument-error') {
-      const { idToken, refreshToken: newRefreshToken } = await fetchNewTokenWithRefreshToken(refreshToken)
-      const newSessionCookie = await auth.createSessionCookie(idToken, { expiresIn: firebaseSessionCookieExpiresIn })
-      const user = await auth.verifySessionCookie(newSessionCookie)
-
-      return {
-        user,
-        newSessionCookie,
-        newRefreshToken,
-      }
-    }
-
-    // re-throw error if not session-cookie-expired error
-    throw e
-  }
-}
