cb48532ef4d48b27a765bfa6d40802a594dfe6d7
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-23 20:56:02 +0900

    update drizzle configs

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from 'drizzle-kit'
-import * as dotenv from 'dotenv'
-
-if (process.env.NEON_BRANCH === 'MAIN') dotenv.config({ path: '../../.env.production.local' })
-else dotenv.config({ path: '../../.env.development.local' })
-
-const dbUrl = process.env.NEON_DATABASE_URL
-if (!dbUrl) throw Error('Database URL not found')
 
 export default defineConfig({
   schema: './schema',
   dialect: 'sqlite',
-  strict: true,
-  verbose: true,
-  dbCredentials: {
-    url: dbUrl,
-  },
 })
