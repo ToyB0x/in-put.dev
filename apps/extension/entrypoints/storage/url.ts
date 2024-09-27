41598a510872fa04cba841d3554a0468720cb89c
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-28 08:07:37 +0900

    update storage

--- apps/extension/entrypoints/storage/url.ts
+++ apps/extension/entrypoints/storage/url.ts
@@ -1,6 +1,9 @@
-type StorageBookmarkV1 = string[] // urls
+type StorageURLv1 = {
+  url: string
+  isMarked: boolean
+}[]
 
-export const storageURLv1 = storage.defineItem<StorageBookmarkV1>('local:bookmark', {
+export const storageURLv1 = storage.defineItem<StorageURLv1>('local:url', {
   fallback: [],
   version: 1,
 })
