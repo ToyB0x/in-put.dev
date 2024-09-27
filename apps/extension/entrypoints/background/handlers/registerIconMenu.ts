872cff17502e80744a2c8586414f0fe5f0c65b43
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-27 12:04:13 +0900

    update register icon menu

--- apps/extension/entrypoints/background/handlers/registerIconMenu.ts
+++ apps/extension/entrypoints/background/handlers/registerIconMenu.ts
@@ -1,3 +1,6 @@
+import client from '@/entrypoints/libs/client'
+import { storageAllowedDomainV1 } from '@/entrypoints/storage/allowedDomain'
+import { updateIcon } from './updateIcon'
 import type { Auth } from 'firebase/auth/web-extension'
 
 const contextMenuId = browser.contextMenus.create({
@@ -7,10 +10,48 @@ const contextMenuId = browser.contextMenus.create({
   contexts: ['action'],
 })
 
+const contextMenuSyncId = browser.contextMenus.create({
+  title: 'sync domain',
+  type: 'normal',
+  id: 'sync-domain' + crypto.randomUUID(),
+  contexts: ['action'],
+})
+
 // Register extension icon context menu
 export const registerIconMenu = (auth: Auth) =>
   browser.contextMenus.onClicked.addListener(async (info, tab) => {
+    // Menu: Sign out
     if (info.menuItemId === contextMenuId) {
       await auth.signOut()
+    }
+
+    // Menu: Sync data
+    if (info.menuItemId === contextMenuSyncId) {
+      if (!auth.currentUser) {
+        console.warn('no user logged in')
+        return
+      }
+
+      const token = await auth.currentUser?.getIdToken()
+      if (!token) throw Error('no token')
+
+      const resDomains = await client.domains.$get(
+        {},
+        {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        },
+      )
+      const dataDomains = await resDomains.json()
+
+      await storageAllowedDomainV1.setValue(dataDomains.domains)
+
+      const [activeTab] = await browser.tabs.query({ active: true, currentWindow: true })
+      const activeUrl = activeTab.url
+      await updateIcon({
+        auth,
+        activeUrl,
+      })
     }
   })
