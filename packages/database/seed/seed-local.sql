f3e73022a22f24386adc2a4c58e434dbf436c8c3
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-27 10:59:54 +0900

    fix seed

--- packages/database/seed/seed-local.sql
+++ packages/database/seed/seed-local.sql
@@ -1,4 +1,4 @@
 INSERT INTO
     user (name, display_name, email, firebase_uid)
 VALUES
-    ('user1', 'user1', 'user1@example.com', 'xxxxx');
+    ('user1', 'user1', 'user1@example.com', 'JwCbJ6cGDcXzGBhmXccn8PY7zGE3');
