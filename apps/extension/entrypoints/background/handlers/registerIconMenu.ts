8c5978ee9c9dd26ccb974e301c948de25b71a1d5
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-28 09:28:04 +0900

    add context menu

--- apps/extension/entrypoints/background/handlers/registerIconMenu.ts
+++ apps/extension/entrypoints/background/handlers/registerIconMenu.ts
@@ -11,6 +11,13 @@ const contextMenuId = browser.contextMenus.create({
   contexts: ['action'],
 })
 
+const contextMenuDisableDomainId = browser.contextMenus.create({
+  title: 'disable domain',
+  type: 'normal',
+  id: 'disable-domain' + crypto.randomUUID(),
+  contexts: ['action'],
+})
+
 const contextMenuSyncId = browser.contextMenus.create({
   title: 'sync',
   type: 'normal',
@@ -21,43 +28,60 @@ const contextMenuSyncId = browser.contextMenus.create({
 // Register extension icon context menu
 export const registerIconMenu = (auth: Auth) =>
   browser.contextMenus.onClicked.addListener(async (info, tab) => {
+    if (!auth.currentUser) {
+      console.warn('no user logged in')
+      return
+    }
+
     // Menu: Sign out
     if (info.menuItemId === contextMenuId) {
       await auth.signOut()
     }
 
+    // Menu: Disable domain
+    if (info.menuItemId === contextMenuDisableDomainId) {
+      const token = await auth.currentUser?.getIdToken()
+      if (!token) throw Error('no token')
+
+      // disable domain
+      const activeTabUrl = tab?.url
+      if (!activeTabUrl) return
+      const resDomainDisable = await client.domains.disable.$post(
+        {
+          json: { domain: new URL(activeTabUrl).hostname },
+        },
+        { headers: { Authorization: 'Bearer ' + token } },
+      )
+
+      const dataResDomainDisable = await resDomainDisable.json()
+      if (!dataResDomainDisable.success) throw Error('failed to disable domain')
+
+      // update domain
+      const resDomains = await client.domains['enabled-domains'].$get(
+        {},
+        { headers: { Authorization: 'Bearer ' + token } },
+      )
+      const dataDomains = await resDomains.json()
+
+      await storageAllowedDomainV1.setValue(dataDomains.domains)
+    }
+
     // Menu: Sync data
     if (info.menuItemId === contextMenuSyncId) {
-      if (!auth.currentUser) {
-        console.warn('no user logged in')
-        return
-      }
-
       const token = await auth.currentUser?.getIdToken()
       if (!token) throw Error('no token')
 
       // update domain
       const resDomains = await client.domains['enabled-domains'].$get(
         {},
-        {
-          headers: {
-            Authorization: 'Bearer ' + token,
-          },
-        },
+        { headers: { Authorization: 'Bearer ' + token } },
       )
       const dataDomains = await resDomains.json()
 
       await storageAllowedDomainV1.setValue(dataDomains.domains)
 
       // update urls
-      const resUrls = await client.urls.$get(
-        {},
-        {
-          headers: {
-            Authorization: 'Bearer ' + token,
-          },
-        },
-      )
+      const resUrls = await client.urls.$get({}, { headers: { Authorization: 'Bearer ' + token } })
       const dataUrls = await resUrls.json()
 
       await storageURLv1.setValue(dataUrls)
