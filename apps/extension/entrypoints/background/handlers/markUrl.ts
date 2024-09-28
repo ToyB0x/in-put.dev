f1c291a95d67cbe6e84735189884509361d8087d
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-09-28 09:27:17 +0900

    add mark url usecase

---
+++ apps/extension/entrypoints/background/handlers/markUrl.ts
@@ -0,0 +1,18 @@
+import { getPureUrl } from '@/entrypoints/libs/getPureUrl'
+import client from '@/entrypoints/libs/client'
+import { auth } from '@/entrypoints/background'
+
+export const markUrl = async ({ url, isMarked }: { url: string; isMarked: boolean }) => {
+  if (!auth.currentUser) {
+    console.warn('no user logged in')
+    return
+  }
+
+  const token = await auth.currentUser.getIdToken()
+  if (!token) throw Error('no token')
+
+  await client.urls.mark.$post(
+    { json: { url: getPureUrl(url), isMarked } },
+    { headers: { Authorization: 'Bearer ' + token } },
+  )
+}
